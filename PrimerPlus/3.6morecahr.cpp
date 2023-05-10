74f14792c0d4166906d20890f05cae1f33779170
无语 <67907645+wuyu1212@users.noreply.github.com>
2023-05-10 14:41:14 +0800

    Add files via upload

---
+++ PrimerPlus/3.6morecahr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char ch = 'M';
+	char hh;
+	int i = ch;
+	cout << ch << i;
+	cout.put(ch);
+	//cin >> hh;
+	//cout << hh;
+	cout << '$';
+
+	return 0;
+ }
